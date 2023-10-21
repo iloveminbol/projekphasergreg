2e20fe1ea51c711232cdf99d6df8d5770fca8b4b
unknown <gregorygladwin4@gmail.com>
2023-10-21 16:31:49 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 
-// import HelloWorldScene from './scenes/HelloWorldScene'
 import CoronaBusterScene from "./scenes/CoronaBusterScene";
+import GameOverScene from "./scenes/GameOverScene";
 
 const config = {
   type: Phaser.AUTO,
@@ -14,7 +14,7 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [CoronaBusterScene],
+  scene: [CoronaBusterScene, GameOverScene],
 };
 
 export default new Phaser.Game(config);
